58dc0dcc5cc35b7b3dad58718a9339288ebeb5fb
Andy Pavlo <pavlo@cs.brown.edu>
2021-04-02 21:51:57 -0400

    Added a basic C++ test for Decimal. This is a work in progress

---
+++ test/basic_test.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <string>
+#include <stdio.h>
+
+#include "gtest/gtest.h"
+#include "decimal.h"
+
+namespace libfixeypointy {
+
+// NOLINTNEXTLINE
+TEST(BasicTests, ToStringTest) {
+  for (int i = 0; i < (int)Decimal::MAX_SCALE - 1; i++) {
+    std::string expected = "9.";
+    for (int j = 0; j <= i; j++) {
+      expected += std::to_string(j % 10);
+    }
+    Decimal d(expected, i + 1);
+    auto result = d.ToString(i + 1);
+    EXPECT_EQ(result, expected);
+  }
+}
+
+// NOLINTNEXTLINE
+TEST(BasicTests, ToStringLeadingZeroTest) {
+  // Make sure we always include a leading zero even if the number is negative
+  std::vector<std::string> strings = { "0.001", "-0.001" };
+  for (auto expected : strings) {
+    Decimal d(expected, 3);
+    auto result = d.ToString(3);
+    EXPECT_EQ(result, expected);
+  }
+}
+
+// NOLINTNEXTLINE
+TEST(BasicTests, AdditionTest) {
+  std::string expected = "3.3";
+  for (int i = 1; i < (int)Decimal::MAX_SCALE - 1; i++) {
+    Decimal d0("1.2", i);
+    Decimal d1("2.1", i);
+    d0 += d1;
+    auto result = d0.ToString(i);
+    EXPECT_EQ(result, expected);
+    expected += "0";
+  }
+}
+
+// NOLINTNEXTLINE
+TEST(BasicTests, SubtractionTest) {
+  std::string expected = "0.0";
+  for (int i = 1; i < (int)Decimal::MAX_SCALE - 1; i++) {
+    std::string value = "9.";
+    for (int j = 0; j <= i; j++) {
+      value += "9";
+    }
+    Decimal d0(value, i);
+    Decimal d1(value, i);
+    d0 -= d1;
+    auto result = d0.ToString(i);
+    EXPECT_EQ(result, expected);
+    expected += "0";
+  }
+}
+
+}  // namespace libfixeypointy
