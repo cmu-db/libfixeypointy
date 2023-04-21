7244429e9b425ecf7947bd3e792529497908a88e
typemoe <moe@pytrade.info>
2023-04-20 21:20:55 -0400

    feat(benchmark) timing of initializers for libfixeypointy

--- src/bench.cpp
+++ src/bench.cpp
@@ -17,59 +17,67 @@ using namespace libfixeypointy;
 
 void Bench(const std::string &mode, const int trials, const int skip, const int iterations,
            const std::string &bench_file) {
-  auto t1 = std::chrono::high_resolution_clock::now();
-  auto t2 = std::chrono::high_resolution_clock::now();
+  auto t_op_start = std::chrono::high_resolution_clock::now();
+  auto t_op_end = std::chrono::high_resolution_clock::now();
 
   for (int trial = 0; trial < trials; ++trial) {
     std::ifstream in(bench_file);
     std::string op1_s;
     std::string op2_s;
-    int64_t time = 0;
+    int64_t init_time = 0;
+    int64_t op_time = 0;
 
     while (in >> op1_s >> op2_s) {
-      std::vector<Decimal> op1_arr;
-      std::vector<Decimal> op2_arr;
+      std::vector<Decimal *> op1_arr;
+      std::vector<Decimal *> op2_arr;
+      op1_arr.resize(iterations);
+      op2_arr.resize(iterations);
       Decimal::ScaleType op1_scale = 0;
       Decimal::ScaleType op2_scale = 0;
+
+      auto t_init_start = std::chrono::high_resolution_clock::now();
       for (int i = 0; i < iterations; ++i) {
-        op1_arr.push_back(Decimal(op1_s, &op1_scale));
-        op2_arr.push_back(Decimal(op2_s, &op2_scale));
+        op1_arr[i] = new Decimal(op1_s, &op1_scale);
+        op2_arr[i] = new Decimal(op2_s, &op2_scale);
       }
+      auto t_init_end = std::chrono::high_resolution_clock::now();
 
       if (mode == "add") {
-        t1 = std::chrono::high_resolution_clock::now();
+        t_op_start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < iterations; ++i) {
-          op1_arr[i].Add(op2_arr[i]);
+          op1_arr[i]->Add(*op2_arr[i]);
         }
-        t2 = std::chrono::high_resolution_clock::now();
+        t_op_end = std::chrono::high_resolution_clock::now();
       } else if (mode == "sub") {
-        t1 = std::chrono::high_resolution_clock::now();
+        t_op_start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < iterations; ++i) {
-          op1_arr[i].Subtract(op2_arr[i]);
+          op1_arr[i]->Subtract(*op2_arr[i]);
         }
-        t2 = std::chrono::high_resolution_clock::now();
+        t_op_end = std::chrono::high_resolution_clock::now();
       } else if (mode == "mlt") {
-        t1 = std::chrono::high_resolution_clock::now();
+        t_op_start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < iterations; ++i) {
-          op1_arr[i].Multiply(op2_arr[i], op2_scale);
+          op1_arr[i]->Multiply(*op2_arr[i], op2_scale);
         }
-        t2 = std::chrono::high_resolution_clock::now();
+        t_op_end = std::chrono::high_resolution_clock::now();
       } else if (mode == "div") {
-        t1 = std::chrono::high_resolution_clock::now();
+        t_op_start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < iterations; ++i) {
-          op1_arr[i].Divide(op2_arr[i], op2_scale);
+          op1_arr[i]->Divide(*op2_arr[i], op2_scale);
         }
-        t2 = std::chrono::high_resolution_clock::now();
+        t_op_end = std::chrono::high_resolution_clock::now();
       } else {
         std::cout << "Unsupported benchmark mode" << std::endl;
       }
 
-      auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-      time += duration.count();
+      auto init_duration = std::chrono::duration_cast<std::chrono::microseconds>(t_init_end - t_init_start);
+      auto op_duration = std::chrono::duration_cast<std::chrono::microseconds>(t_op_end - t_op_start);
+      init_time += init_duration.count();
+      op_time += op_duration.count();
     }
 
     if (trial >= skip) {
-      std::cout << time << std::endl;
+      std::cout << init_time << "," << op_time << std::endl;
     }
   }
 }
